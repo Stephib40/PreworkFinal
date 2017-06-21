6d4885c43c7b9242b632a508ad0ab2499c78c580
Steph Banks <Stephibanks4.0@gmail.com>
2017-06-21 12:01:41 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreworkFinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter a Number to Reverse");
+            int Number = int.Parse(Console.ReadLine());
+            int Reverse = 0;
+            while (Number > 0)
+            {
+                int remainder = Number % 10;
+                Reverse = (Reverse * 10) + remainder;
+                Number = Number / 10;
+            }
+            Console.WriteLine("Your Reverse Number is {0}", Reverse);
+            Console.ReadLine();
+        }
+    }
+    }
+}
